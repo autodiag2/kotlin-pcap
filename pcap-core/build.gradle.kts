bc5ada5990a4ec6b45be393237a7a5c97949eb1e
autodiag2 <autodiag@netcourrier.com>
2026-05-28 12:56:47 +0200

    fix

--- pcap-core/build.gradle.kts
+++ pcap-core/build.gradle.kts
@@ -1,37 +1,32 @@
 plugins {
-    kotlin("multiplatform")
-    id("maven-publish")
+    kotlin("android")
     id("com.android.library")
+    id("maven-publish")
 }
 
 group = "com.github.autodiag2"
 version = property("VERSION_NAME") as String
 
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
 kotlin {
-    jvm()
-
-    androidTarget()
-
-    js(IR) {
-        browser()
-        nodejs()
-    }
-
-    sourceSets {
-        commonMain.dependencies {
-        }
-
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-        }
-    }
+    jvmToolchain(21)
 }
 
 android {
     namespace = "pcap.core"
+
     compileSdk = 36
 
     defaultConfig {
         minSdk = 24
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 }
+
