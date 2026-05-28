f22976b65e46b233f7106b2dac3da0199c45339b
autodiag2 <autodiag@netcourrier.com>
2026-05-28 12:24:51 +0200

    jitpack integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,8 @@ plugins {
     id("com.android.library") version "8.10.0" apply false
     id("maven-publish")
 }
+subprojects { 
+    apply(plugin = "maven-publish")
+    group = rootProject.group
+    version = rootProject.version 
+}
